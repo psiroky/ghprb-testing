98ba56874df52f05680ca740fbe60bd75a7fccd0
Petr Široký <siroky.petr@gmail.com>
2017-08-18 16:25:35 +0200

    blabla

--- src/main/java/org/psiroky/test/App.java
+++ src/main/java/org/psiroky/test/App.java
@@ -6,7 +6,7 @@ package org.psiroky.test;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) blabla
     {
         System.out.println( "Hello World!" );
     }
